f0f1b8e7438b59b1bfd0f97783c3e3b25acc2da5
THIPPI REDDY VENU PRAKASH REDDY <49930985+venuprakash17@users.noreply.github.com>
2021-12-09 01:50:10 +0530

    transpose,presed_key-->Created
    transpose:
    it will create a list of all the lists present in the gridCells
    pressed_key:
    will be the event for the bind in tkinter here we are using the functions created in the Board class for the triggers in the game class

--- 2048.py
+++ 2048.py
@@ -36,6 +36,10 @@ class Board:
         self.GameZone = Frame(self.game_panel, bg='azure3')
         self.gridCell = [[0] * 4 for i in range(4)]
         self.board = []
+        self.compress = False
+        self.merge = False
+        self.moved = False
+        self.score = 0
         for i in range(4):
             rows = []
             for j in range(4):
@@ -53,6 +57,18 @@ class Board:
                 (self.gridCell[ind][i], self.gridCell[ind][j]) =( self.gridCell[ind][j], self.gridCell[ind][i])
                 i += 1
                 j -= 1    
+    def Shift_Opposite(self):
+        for ind in range(4):
+            i = 0
+            j = 3
+            while (i < j):
+                (self.gridCell[ind][i], self.gridCell[ind][j]) =( self.gridCell[ind][j], self.gridCell[ind][i])
+                i += 1
+                j -= 1
+
+    def transpose(self):
+        self.gridCell = [list(t) for t in zip(*self.gridCell)]
+
     def Grid_Compress(self):
         temp = [[0] * 4 for i in range(4)]
         for i in range(4):
@@ -62,6 +78,7 @@ class Board:
                     temp[i][cnt] = self.gridCell[i][j]
                     cnt += 1
         self.gridCell = temp
+
     def Grid_Merge(self):
         for i in range(4):
             for j in range(4 - 1):
@@ -69,21 +86,68 @@ class Board:
                     self.gridCell[i][j] *= 2
                     self.gridCell[i][j + 1] = 0
                     self.score += self.gridCell[i][j]
-                    
 
+
+    def random_cell(self):
+        cells = []
+        for i in range(4):
+            for j in range(4):
+                if self.gridCell[i][j] == 0:
+                    cells.append((i, j))
+        curr = random.choice(cells)
+        i = curr[0]
+        j = curr[1]
+        lst = [2, 4]
+        self.gridCell[i][j] = random.choice(lst)
 class Game:
     def __init__(self, gameWindow):
         self.gameWindow = gameWindow
         self.end = False
         self.won = False
-    #
+    
+   
+    def link_keys(self, event):
+        if self.end or self.won:
+            return
 
-    def start(self):
-        # self.gameWindow.random_cell()
-        # self.gameWindow.random_cell()
-        # self.gameWindow.grid_paint()
-        self.gameWindow.game_panel.bind('<Key>', True)
-        self.gameWindow.game_panel.mainloop()   
+        self.gameWindow.compress = False
+        self.gameWindow.merge = False
+        self.gameWindow.moved = False
+
+        presed_key = event.keysym
+
+        if presed_key == 'Up' :
+            self.gameWindow.transpose()
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.Grid_Merge()
+            
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.transpose()
+
+        elif presed_key == 'Down':
+            self.gameWindow.transpose()
+            self.gameWindow.Shift_Opposite()
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.Grid_Merge()
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.Shift_Opposite()
+            self.gameWindow.transpose()
+
+        elif presed_key == 'Left':
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.Grid_Merge()
+            self.gameWindow.Grid_Compress()
+
+        elif presed_key == 'Right':
+            self.gameWindow.Shift_Opposite()
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.Grid_Merge()
+            self.gameWindow.Grid_Compress()
+            self.gameWindow.Shift_Opposite()
+
+        else:
+            pass
+
 
 gameWindow = Board()
 game2048 = Game(gameWindow)
